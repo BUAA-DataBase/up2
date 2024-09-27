5c454025c9ea79bf5b083849d7ca57014a550773
周子皓 <1392676693@qq.com>
2024-09-27 16:46:54 +0800

    Initial Commit

---
+++ up2/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  up2
+//
+//  Created by 周子皓 on 2024/9/27.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ up2/up2App.swift
@@ -0,0 +1,17 @@
+//
+//  up2App.swift
+//  up2
+//
+//  Created by 周子皓 on 2024/9/27.
+//
+
+import SwiftUI
+
+@main
+struct up2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
